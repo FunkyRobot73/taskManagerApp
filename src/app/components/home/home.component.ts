173b27c15832dd70130c9eb125f798eb2a4e4618
Carlos <arlo252@gmail.com>
2023-03-28 18:16:01 -0400

    Data displaying & Directives

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  photo: String = "https://cdn.xxl.thumbs.canstockphoto.com/glass-of-water-on-a-white-background-pictures_csp22607686.jpg";
+  constructor() { }
+
+  ngOnInit() {
+
+  }
 
 }
