9bf8d104ba80b876f9d1e7b41501a4e64810b0a1
Carlos <arlo252@gmail.com>
2023-04-05 18:06:48 -0400

    Form Builder & Getter edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { HomeComponent } from './components/home/home.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'tasks', component: TasksComponent},
+  {path: 'add-task', component: AddTaskComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
